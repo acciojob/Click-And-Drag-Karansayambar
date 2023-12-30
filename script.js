3bef2d1d1fc11551da0a621a61f7cba0ab7d320b
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-30 12:10:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // Your code here.
+let draggedItem;
+
+  function drag(event) {
+    draggedItem = event.target;
+    event.dataTransfer.setData("text", event.target.textContent);
+  }
+
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+
+  function drop(event) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text");
+    const targetItem = document.elementFromPoint(event.clientX, event.clientY);
+    
+    if (targetItem && targetItem.classList.contains("item")) {
+      targetItem.insertAdjacentHTML("beforebegin", `<div class="item" draggable="true" ondragstart="drag(event)">${data}</div>`);
+      draggedItem.remove();
+    }
+  }
